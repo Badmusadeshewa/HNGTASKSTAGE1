2c8f69055e7955d695d780a4e0240689395d6d51
Badmusadeshewa <ewastitches1@gmail.com>
2023-09-11 21:53:28 +0100

    retrying the api path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/info', (req, res) => {
+app.get('/api/slack_name&track', (req, res) => {
   const slackName = 'Adeshewa Badmus';
   const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
   const utcTime = new Date().toUTCString();
